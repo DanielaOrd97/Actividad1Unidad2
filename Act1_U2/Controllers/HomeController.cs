bae41f360f3cf875262aef7876182e5bac79741d
Daniela Ordoñez <201G0272@rcarbonifera.tecnm.mx>
2023-10-01 18:04:38 -0600

    Index

---
+++ Act1_U2/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Act1_U2.Models;
+using Act1_U2.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Act1_U2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ZooplanetContext context = new();
+
+            var d = context.Clase.OrderBy(x => x.Nombre).Select(x => new IndexViewModel
+            {
+                Id = x.Idclase,
+                Nombre = x.Nombre ?? ""
+            });
+
+            return View(d);
+        }
+    }
+}
---
+++ Act1_U2/Models/ViewModel/IndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Act1_U2.Models.ViewModel
+{
+    public class IndexViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+
+    }
+}
